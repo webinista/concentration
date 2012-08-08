8db62358f9b3550c04cbaaa974b3e2a057533e91
Tiffany B. Brown <tiffany@webinista.com>
2012-08-08 14:46:51 -0700

    Basic functionality is complete.

--- concentration.js
+++ concentration.js
@@ -1,96 +1,59 @@
-var pairs = 8; // let the user set this. should it a square number? 4-by-x row?
+/* TO DOs:
+
+- Add touch support
+- Add a way to inject a style information/sheet if 3D
+  transforms aren't supported
+- Wrap so can use as an obj
+*/
+
+var conf = {};
+conf.countdown = 3;
+conf.pairs = 3; // set a default
 
 var deck1,
     deck2,
-    doesmatch,
     curpair = [],
-    size,
-    cards = 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z'.split(',');
-
-size = cards.splice(0,pairs);
-
-/* Shuffle function from http://stackoverflow.com/questions/962802/is-it-correct-to-use-javascript-array-sort-method-for-shuffling */
-
-Array.prototype.shuffle = function() {
-    var tmp, current, top = this.length;
-    if(top) {
-        while(--top) {
-            current = Math.floor(Math.random() * (top + 1));
-            tmp = this[current];
-            this[current] = this[top];
-            this[top] = tmp;
-        }
-    }
-    return this;
-}
-
-Array.prototype.copy = function(){
-    var i, len = this.length, copy = [];
-    for(i = 0; i < len; i++){
-        copy[i] = this[i];
-    }
-    return copy;
-}
-
-deck1 = size.shuffle();
-deck2 = size.copy().shuffle();
-
-for(var j=0; j < deck1.length; j++){
-    var cardA = document.createElement('div');
-    var cardB = document.createElement('div');
-
-    var valueA = document.createTextNode( deck1[j]);
-    var valueB = document.createTextNode( deck2[j]);
-
-    cardA.appendChild(valueA);
-    cardB.appendChild(valueB);
-
-    cardA.setAttribute('data-cardvalue',deck1[j]);
-    cardB.setAttribute('data-cardvalue',deck2[j]);
-
-    cardA.setAttribute('class','card');
-    cardB.setAttribute('class','card');
-
-
-    document.body.appendChild(cardA);
-    document.body.appendChild(cardB);
-}
-
-var stack = document.getElementsByTagName('div');
-
-// Setting on both NodeList and HTMLCollection
-NodeList.prototype.setHandler = HTMLCollection.prototype.setHandler = function(event, func, useCapture){
-    for(var i =0; i < this.length; i++){
-        this[i].addEventListener(event, func, useCapture );
-    }
-}
+    cards = 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z'.split(','),
+    start,
+    end,
+    numtries = 0,
+    config = document.getElementById('config'),
+    deckwrapper = document.getElementById('deck');
 
 /* Using event delegation here. That might not be ideal. */
-window.addEventListener('click', function(e){
-    if( e.target.toString() == '[object HTMLDivElement]' ){
+
+var onclick = function(e){
+    if( e.target.parentNode.classList.contains('card') ){
         var cp = curpair;
 
-        /* Show the card */
-        e.target.classList.add('flipped');
+        /* If we have flipped it over, don't let be flipped again. */
+        if( e.target.parentNode.classList.contains('flipped') === false ){
 
-        /* clear out current pair */
-        if( cp.length >= 2){
-            while( cp.length ){
-                cp.pop();
+            /* Show the card */
+            e.target.parentNode.classList.add('flipped');
+
+            /* Clear out current pair */
+            if( cp.length >= 2){
+                while( cp.length ){
+                    cp.pop();
+                }
+            }
+            cp.push( e.target.parentNode.dataset.cardvalue );
+
+            /* if we have a pair to compare */
+            if( cp.length == 2){
+                // this works for now, but can we use a transition end instead?
+                setTimeout( function(){ doesmatch( cp[0], cp[1] ) }, 500);
             }
         }
-
-        cp.push( e.target.dataset.cardvalue );
-
-        /* if we have a pair to compare */
-        if( cp.length == 2){
-            doesmatch( cp[0], cp[1] );
-        }
     }
-},false);
+}
 
-doesmatch = function(a,b){
+var doesmatch = function(a,b){
     var matches, matchevt;
+
+    // increment the number of tries
+    numtries++;
 
     if( arguments.length == 2){
         (a === b) ? matches = 'matches' : matches = 'resetcards';
@@ -101,43 +64,176 @@ doesmatch = function(a,b){
     }
 }
 
-var onMatch = function(e){
+var onmatch = function(e){
     var these = document.getElementsByClassName('flipped');
     var len = these.length;
     for(var i=0; i < len; i++){
         these[i].classList.add('matched');
     }
+
     /* Reset the list of flipped items */
     window.dispatchEvent( new Event('resetcards') );
 
+    /* Check whether we should end the game */
     isdone();
 }
 
-var onReset = function(e){
-    /* Using querySelectorAll even tho it's slower
-       because we need a static list, not a
-       dynamic one. Otherwise, the second flipped item
-       won't be unflipped.
-    */
+var onreset = function(e){
     var these = document.getElementsByClassName('card');
+
     for(var i=0; i < these.length; i++){
         if( these[i].classList.contains('flipped') ){
             these[i].classList.remove('flipped');
         }
     }
+
 }
 
 var isdone = function(){
+    /* Checks whether we have matched all pairs */
     var matchedpairs = document.getElementsByClassName('matched').length / 2;
-    if(matchedpairs == pairs){
+    if(matchedpairs == conf.pairs){
         window.dispatchEvent( new Event('stoptime') );
     }
 }
 
-var onStop = function(){
-    console.log('stop clock');
+var onstart = function(){
+    document.getElementById('overlay').classList.add('hide');
+    document.getElementById('countdown').classList.add('hide');
+    document.getElementById('deck').classList.remove('hide');
+    start = Date.now();
 }
 
-window.addEventListener('matches', onMatch, false);
-window.addEventListener('resetcards', onReset, false);
-window.addEventListener('stoptime', onStop, false);
+var onstop = function(){
+    var scoreevt;
+    end = Date.now();
+
+    scoreevt = new Event('tallyscore');
+    window.dispatchEvent(scoreevt);
+
+    window.removeEventListener('click', onclick ,false);
+}
+
+var shuffle = function(numpairs){
+    var cnf = conf;
+    cnf.pairs = numpairs;
+    /* Shuffle the cards, pick a set,
+       copy the set and then shuffle it */
+    var c = cards.shuffle(),
+        deck1 = c.splice(0,cnf.pairs),
+        deck2 = deck1.copy().shuffle(),
+
+        /* Get the first figure to use as a template */
+        tpl = deckwrapper.firstElementChild,
+        cardA, cardB, valueA, valueB, cd;
+
+    for(var j=0; j < deck1.length; j++){
+        /* Need to copy TPL and replace the items */
+
+        cardA = tpl.cloneNode(true);
+        cardB = cardA.cloneNode(true);
+
+        cardA.setAttribute('data-cardvalue',deck1[j]);
+        cardB.setAttribute('data-cardvalue',deck2[j]);
+
+        valueA = document.createTextNode( deck1[j] );
+        valueB = document.createTextNode( deck2[j] );
+
+        cardA.firstElementChild.replaceChild(valueA,cardA.firstElementChild.firstChild );
+        cardB.firstElementChild.replaceChild(valueB,cardB.firstElementChild.firstChild );
+
+        deckwrapper.appendChild( cardA );
+        deckwrapper.appendChild( cardB );
+    }
+
+    /* Remove the template node from the stack */
+    deckwrapper.removeChild( tpl ).childNodes;
+
+    /* Add an event handler for the start and stop time events */
+    window.addEventListener('starttime', onstart, false);
+    window.addEventListener('stoptime', onstop, false);
+
+    cd =  new Event('countdown');
+    window.dispatchEvent( cd );
+}
+
+var countdown = function(){
+    var sec = conf.countdown,
+        cd,
+        s,
+        cui = document.getElementById('countdown');
+
+    cui.classList.remove('hide');
+
+    cd = setInterval( function(){
+        /* When the countdown is over...*/
+        if( sec == 0 ){
+            s = document.createTextNode('GO!');
+        } else if( sec == -1){
+            window.dispatchEvent( new Event('starttime') );
+            clearInterval(cd);
+        } else {
+            s = document.createTextNode(sec);
+        }
+        cui.replaceChild(s,cui.firstChild);
+        sec--;
+
+    }, 1000);
+
+    /* Add click handler for our cards */
+    window.addEventListener('click', onclick ,false);
+
+    /* Add a score for when we need to tally it.*/
+    window.addEventListener('tallyscore', ontallyscore, false);
+
+    /* When the game ends and we want to save the score.*/
+    window.addEventListener('savescore', onsavescore, false);
+
+    /* When we match a pair */
+    window.addEventListener('matches', onmatch, false);
+
+    /* When we don't match a pair */
+    window.addEventListener('resetcards', onreset, false);
+
+
+}
+
+var ontallyscore = function(e){
+    var st = start, e = end, nt = numtries, p = conf.pairs, score, savescore;
+    /*
+       calculate the score.
+       pairs / number of tries * length of time * 5.
+    */
+    score = ( p / nt ) * (e - st) * 5;
+
+    sendscore = new Event('savescore');
+    sendscore.score = Math.floor( score );
+    window.dispatchEvent(sendscore);
+}
+
+var onsavescore = function(e){
+
+    document.getElementById('overlay').classList.remove('hide');
+    document.getElementById('score').classList.remove('hide');
+
+  /* If we have Local Storage available, offer the option to save the score */
+    if(Lib.hasLocalStorage()){
+        console.log('Save this to localstorage');
+    }
+}
+
+var onsubmit = function(e){
+    e.preventDefault();
+    e.target.classList.add('hide');
+
+    shuffle( e.target['pairs'].value );
+}
+
+var init = function(e){    /* Add an event listener to the configuration form. */
+    config.addEventListener('submit', onsubmit, false);
+}
+
+window.addEventListener('DOMContentLoaded', init, false);
+window.addEventListener('countdown', countdown, false);
+
+
---
+++ helpers.js
@@ -0,0 +1,39 @@
+/* Shuffle function from http://stackoverflow.com/questions/962802/is-it-correct-to-use-javascript-array-sort-method-for-shuffling */
+
+Array.prototype.shuffle = function() {
+    var tmp, current, top = this.length;
+    if(top) {
+        while(--top) {
+            current = Math.floor(Math.random() * (top + 1));
+            tmp = this[current];
+            this[current] = this[top];
+            this[top] = tmp;
+        }
+    }
+    return this;
+};
+
+Array.prototype.copy = function(){
+    var i, len = this.length, copy = [];
+    for(i = 0; i < len; i++){
+        copy[i] = this[i];
+    }
+    return copy;
+};
+
+
+// Setting on both NodeList and HTMLCollection
+NodeList.prototype.setHandler = HTMLCollection.prototype.setHandler = function(event, func, useCapture){
+    for(var i =0; i < this.length; i++){
+        this[i].addEventListener(event, func, useCapture );
+    }
+};
+
+window.Lib = (function(global){
+    return {
+        hasLocalStorage: function(){
+            return ( global.localStorage === undefined ) && ( Object.prototype.toString.call(global.localStorage) === "[object Storage]");
+        }
+    };
+
+})(window);
